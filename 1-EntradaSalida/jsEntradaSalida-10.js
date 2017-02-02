bdaa44e5f2db6341ff6e17622a9cd25882482c70
Crisitan Sielach <Crisitan Sielach>
2017-02-02 16:28:53 -0300

    completo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe
+	var resultado
+	var msj
+	var importef
+	importe=document.getElementById("importe").value
+	importef=parseInt(importe)
+	resultado=importef-(importef*0.25)
+	document.getElementById("resultado").value=resultado
+
+
+
 }
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,8 @@
 function Mostrar()
 {
 //tomo la edad  
-
+if(document.getElementById("edad").value<13 || document.getElementById("edad").value>17){
+	alert("Usted NO es adolecente")
+}
 
 }//FIN DE LA FUNCIÓN
